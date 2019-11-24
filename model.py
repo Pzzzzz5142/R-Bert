6ef5fbf06a06a67e173d554c3434a11f61d8dc2c
译 张 <hello-cd.plus@hotmail.com>
2019-11-24 11:14:20 +0800

    add gitignore

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
 from transformers import *
 import torch
-from torch import torch.nn as nn
+import torch.nn as nn
 
 
-class MyBert(nn):
+
+class MyBert(nn.Module):
     def __init__(self):
         super(MyBert, self).__init__()
 
-        self.brt = BertModel().from_pretrained('bert-base-uncased')
-        
+        self.brt = BertModel.from_pretrained('bert-base-uncased')
+        self.hidden1=nn.Linear(128,784)
