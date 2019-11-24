aed5cb0ff5b387a26566442ad1a67df891928de9
译 张 <hello-cd.plus@hotmail.com>
2019-11-24 18:01:35 +0800

    真令人头秃

--- DataLoader.py
+++ DataLoader.py
@@ -4,55 +4,63 @@ import numpy as np
 import os
 
 
-class DataLoader(object):
-    def __init__(self, path, readNum):
-        self.readNum = readNum
-        self.path = path
+class InputFeatures(object):
+    """
+    A single set of features of data.
 
-    def load(self):
-        datas, labels = self.__read__()
-        for i in range(len(datas)):
-            for j in range(len(datas[i])):
-                if datas[i][j] == '<':
-                    if datas[i][j+2] == '1':
-                        datas[i] = datas[i][:j]+' $  '+datas[i][j+4:]
-                    elif datas[i][j+2] == '2':
-                        datas[i] = datas[i][:j]+' #  '+datas[i][j+4:]
-                    elif datas[i][j+3] == '1':
-                        datas[i] = datas[i][:j]+' $   '+datas[i][j+5:]
-                    elif datas[i][j+3] == '2':
-                        datas[i] = datas[i][:j]+' $   '+datas[i][j+5:]
-                        break
-        for i in range(len(labels)):
-            for j in range(len(labels)):
-                if labels[i][j] == '(':
-                    labels[i] = labels[i][:j]
-                    break
-        return datas, labels
+    Args:
+        input_ids: Indices of input sequence tokens in the vocabulary.
+        attention_mask: Mask to avoid performing attention on padding token indices.
+            Mask values selected in ``[0, 1]``:
+            Usually  ``1`` for tokens that are NOT MASKED, ``0`` for MASKED (padded) tokens.
+        token_type_ids: Segment token indices to indicate first and second portions of the inputs.
+    """
 
-    def __read__(self):
-        with open(self.path) as fl:
-            reader = csv.reader(fl, delimiter='\t')
-            cnt = 0
-            datas = []
-            labels = []
-            for row in reader:
-                if cnt == 0:
-                    datas.append(row[1])
-                elif cnt == 1:
-                    labels.append(row[0])
-                cnt += 1
-                if cnt == 4:
-                    cnt = 0
-
-        return datas, labels
+    def __init__(self, input_ids, attention_mask, label_id, e1_mask, e2_mask):
+        self.input_ids = input_ids
+        self.attention_mask = attention_mask
+        #self.token_type_ids = token_type_ids
+        self.label_id = label_id
+        self.e1_mask = e1_mask
+        self.e2_mask = e2_mask
 
 
-if __name__ == "__main__":
-    a = 'list(range<e1>(10))122112'
-    for i in range(len(a)):
-        if a[i] == '<':
-            a = a[:i]+' & '+a[i+4:]
-            break
+def load_datas(path, tokenizer, max_len, pad_token=0, add_special_token=True):
+    features = []
+    labels = open(path+'/label.txt').read().split('\n')
+    with open(path+'/train.tsv') as open_file:
+        for line in open_file.readlines():
+            line = line.split('\t')
+            label = line[0]
+            text = line[1]
+            tokened = tokenizer.tokenize(
+                ('[CLS] ' if add_special_token else '') + text)
+            e1_b = tokened.index('<e1>')
+            e1_e = tokened.index('</e1>')
+            e2_b = tokened.index('<e2>')
+            e2_e = tokened.index('</e2>')
+            tokened[e1_b]='$'
+            tokened[e1_e]='$'
+            tokened[e2_b]='#'
+            tokened[e2_e]='#'
+            input_ids = tokenizer.convert_tokens_to_ids(
+                tokened, max_len=max_len)
+            attention_mask = [1 if pad_token == 0 else 1]*len(input_ids)
+            padding_length = max_len-len(input_ids)
+            attention_mask += [pad_token]*padding_length
+            input_ids += [pad_token]*padding_length
+            assert len(input_ids) == max_len
+            assert len(attention_mask) == max_len
+            features.append(InputFeatures(input_ids, attention_mask, label_id=labels.index(label),
+                                          e1_mask=(e1_b, e1_e), e2_mask=(e2_b, e2_e)))
 
-    print(a)
+    return features
+
+
+if __name__ == '__main__':
+    import torch
+    import transformers
+    token = transformers.BertTokenizer.from_pretrained('bert-base-uncased')
+    token.add_special_tokens({'additional_special_tokens': [
+                             '<e1>', '<e2>', '</e1>', '</e2>']})
+    load_datas('./BERT/data', max_len=128, tokenizer=token)
--- train.py
+++ train.py
@@ -1,2 +1,29 @@
-from DataLoader import DataLoader
+import torch
+from torch.utils.data import DataLoader, Dataset, RandomSampler
+from tqdm import tqdm, trange
+import torch.nn as nn
 
+from transformers import *
+
+
+def train(train_dataset, model, tokenizer, num_train_epochs, lr=2e-5, batch_size=16):
+    train_sampler = RandomSampler(train_dataset)
+    train_dataloader = DataLoader(
+        train_dataset, sampler=train_sampler, batch_size=batch_size)
+    optimizer = AdamW(model.parameters(), lr=lr)
+    loss_F = nn.CrossEntropyLoss()
+    train_iterator = trange(int(num_train_epochs), desc='Epoch')
+    for _ in train_iterator:
+        epoch_iterator = tqdm(train_dataloader, desc="Iteration")
+        for step, batch in enumerate(epoch_iterator):
+            inputs = torch.tensor([i.input_ids for i in batch]).cuda()
+            labels = torch.tensor([i.label_id for i in batch]).cuda()
+            attention_mask = torch.tensor(
+                [i.attention_mask for i in batch]).cuda()
+            outputs = model(inputs, attention_mask=attention_mask)
+
+            loss = loss_F(outputs, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
